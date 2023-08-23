4ecd1e52290ec6656ce34672788087affe3e1823
Gunnar97 <135347097+Gunnar97@users.noreply.github.com>
2023-08-24 02:06:38 +0300

    2

--- src/test.js
+++ src/test.js
@@ -1,54 +1,28 @@
-import axios from 'axios';
-import Notiflix from 'notiflix';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-
-
-
-const URL = 'https://pixabay.com/api/'
-const KEY ='39007065-0db3fa1240dd246ce2d69362f'
-const axios = require('axios').default;
-let currentPage = 1;
-let totalEl;
-let q ='';
-
-const refs = {
-    searchForm: document.querySelector('.search-form'),
-    gallery: document.querySelector('.gallery'),
-    input: document.querySelector('[name="searchQuery"]'),
-    target: document.querySelector('.js-guard')
-};
-
-refs.searchForm.addEventListener('submit', onSubmit);
-
 function onSubmit(eve) {
     eve.preventDefault();
     q = refs.input.value;
     console.log(q)
     if (!refs.input.value) return;
-    currentPage = 1;
-    refs.gallery.innerHTML = ''
     fetchPhoto();
-    refs.input.value = '';
+    // refs.input.value = '';
 };
  
 
+
 async function fetchPhoto() {
-  console.log(currentPage);
+  console.log(maxPage);
   const PARAMS = new URLSearchParams({
     key: KEY,
     q: q,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
-    page: currentPage,
+    page: maxPage,
     per_page: 40,
   });
   try {
    const response = await axios.get(`${URL}?${PARAMS}`);
       const images = response.data.hits;
-      totalEl = response.data.total;
-      console.log(totalEl)
       console.log(images)
       renderList(images);
       return images;
@@ -61,6 +35,9 @@ async function fetchPhoto() {
     );
     }
 }
+
+
+
 
 function renderList(arr) {
     if (Array.isArray(arr)) {
@@ -84,43 +61,15 @@ function renderList(arr) {
 </div>
 </a>
 </li>`).join('')
-        refs.gallery.insertAdjacentHTML('beforeend', listPhoto);
+        refs.gallery.innerHTML = listPhoto;
         let lightbox = new SimpleLightbox('.gallery a', {
             fadeSpeed: 800,
             captionsData: 'alt',
             signmentPosition: 'bottom',
             signmentDelay: 250
-        });
-        observer.observe(refs.target);
-        
+        })
     }
     else {
         console.error("Invalid data format: arr is not an array.");
     };
 };
-
-
-let options = {
-    root: null,
-    rootMargin: "400px",
-    threshold: 1.0,
-};
-
-let observer = new IntersectionObserver(onLoad, options);
-function onLoad(entries, observer) {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            currentPage += 1;
-            if (refs.gallery.childElementCount >= totalEl || refs.gallery.childElementCount >= 500) {
-            observer.unobserve(refs.target)
-            Notiflix.Report.info("info", "We're sorry, but you've reached the end of search results.")
-            }
-            else {
-                fetchPhoto()
-            }
-            
- 
-        }
-    });
-    
-}
